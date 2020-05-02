7a23c15e0000f645b33e77bf8b48aad515e2f793
Daniele Sergio <daniele.sergio@kynetics.com>
2020-05-02 19:07:21 +0200

    chore: Add license header to all files
    Add license header to all files

--- entities/src/main/java/com/danielesergio/tabu/entities/Rule.kt
+++ entities/src/main/java/com/danielesergio/tabu/entities/Rule.kt
@@ -1,3 +1,24 @@
+/*
+ * Tabu is a card game where you must guess the main word without using the tabu words
+ *
+ * Copyright (c) 2020 Daniele Sergio
+ *
+ * This file is part of Tabu.
+ *
+ * Tabu is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Tabu is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.danielesergio.tabu.entities
 
 interface Rule {
