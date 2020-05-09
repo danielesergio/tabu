c53a3a1dd694bbc2161a46a9a4f4da6b858f6a22
Daniele Sergio <daniele.sergio@kynetics.com>
2020-05-09 13:56:17 +0200

    feat: Implement game confgiuration use case
    Implement game confgiuration use case

--- usecases/src/main/java/com/danielesergio/tabu/usecases/gameconfiguration/GameConfigurationController.kt
+++ usecases/src/main/java/com/danielesergio/tabu/usecases/gameconfiguration/GameConfigurationController.kt
@@ -3,14 +3,14 @@
  *
  * Copyright (c) 2020 Daniele Sergio
  *
- * This file is part of Tabu.
+ * This file is part of tabu.
  *
- * Tabu is free software: you can redistribute it and/or modify
+ * tabu is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Tabu is distributed in the hope that it will be useful,
+ * tabu is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
@@ -19,11 +19,8 @@
  * along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-package com.danielesergio.tabu.usecases
+package com.danielesergio.tabu.usecases.gameconfiguration
 
-import com.danielesergio.tabu.entities.Rule
-import com.danielesergio.tabu.usecases.entites.RuleImpl
-import com.danielesergio.tabu.usecases.gameconfiguration.RuleData
-
-internal fun RuleData.toRule(): Rule = RuleImpl(pointLimit, pass, resetEveryRound)
-
+interface GameConfigurationController{
+    fun chooseRule(ruleData: RuleData)
+}
